3638012e34fe15a48a8c8a59cb970780249c6082
yiminh <35590279+yiminh@users.noreply.github.com>
2021-06-15 11:50:50 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '530f3e7805674a8695515536f0b00446',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'YiMin Huang',
+  domain: 'https://www.notion.so/YiMin-Huang-530f3e7805674a8695515536f0b00446',
+  author: 'YiMin Huang',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'PoliSci@Taiwan',
+  socialImageTitle: 'YiMin',
+  socialImageSubtitle: '你好 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'yiminhuang95',
+  github: 'yiminh',
+  linkedin: 'yi-min-huang-88136285',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
